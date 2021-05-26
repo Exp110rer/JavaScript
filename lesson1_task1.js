8c70aa4dfa98eae9e3bfae645f1a7cce2125ca27
Exp110rer <73857939+Exp110rer@users.noreply.github.com>
2021-05-27 00:24:51 +0300

    Create lesson1_task1.js

---
+++ lesson1_task1.js
@@ -0,0 +1,6 @@
+var temperatureCelsius = prompt("Введите температуру по Цельсию: ");
+var temperatureFahrenheit = (9 / 5) * temperatureCelsius + 32;
+alert("Температура по Фаренгейту равна: " + temperatureFahrenheit);
+
+// Выводим сообщение для node.js или консоли
+console.log("Температура по Фаренгейту равна: " + temperatureFahrenheit);
